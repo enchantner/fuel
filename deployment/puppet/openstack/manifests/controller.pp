011bb78402385b3f3174deb5955727ec519c0d43
Dmitry Kalashnik <dkalashnik@mirantis.com>
2013-06-14 14:52:01 +0400

    Add Qpid to cluster HA deployment

--- deployment/puppet/openstack/manifests/controller.pp
+++ deployment/puppet/openstack/manifests/controller.pp
@@ -86,6 +86,8 @@ class openstack::controller (
   # not sure if this works correctly
   $internal_address,
   $admin_address,
+  # AMQP
+  $queue_provider          = 'rabbitmq',
   # Rabbit
   $rabbit_password         = 'rabbit_pw',
   $rabbit_user             = 'nova',
@@ -94,6 +96,13 @@ class openstack::controller (
   $rabbit_node_ip_address  = undef,
   $rabbit_ha_virtual_ip    = false, #Internal virtual IP for HA configuration
   $rabbit_port             = '5672',
+  # Qpid
+  $qpid_password           = 'qpid_pw',
+  $qpid_user               = 'nova',
+  $qpid_cluster            = false,
+  $qpid_nodes              = [$internal_address],
+  $qpid_port               = '5672',
+  $qpid_node_ip_address    = undef,
   # network configuration
   # this assumes that it is a flat network manager
   $network_manager         = 'nova.network.manager.FlatDHCPManager',
@@ -180,8 +189,8 @@ class openstack::controller (
 
   $rabbit_addresses = inline_template("<%= @rabbit_nodes.map {|x| x + ':5672'}.join ',' %>")
     $memcached_addresses =  inline_template("<%= @cache_server_ip.collect {|ip| ip + ':' + @cache_server_port }.join ',' %>")
- 
-  
+
+
   nova_config {'DEFAULT/memcached_servers':  value => $memcached_addresses; }
 
   ####### DATABASE SETUP ######
@@ -284,7 +293,7 @@ class openstack::controller (
   }
   else {
     $enabled_apis = 'ec2,osapi_compute,osapi_volume'
-  } 
+  }
 
   class { 'openstack::nova::controller':
     # Database
@@ -320,6 +329,8 @@ class openstack::controller (
     nova_db_password        => $nova_db_password,
     nova_db_user            => $nova_db_user,
     nova_db_dbname          => $nova_db_dbname,
+    # AMQP
+    queue_provider          => $queue_provider,
     # Rabbit
     rabbit_user             => $rabbit_user,
     rabbit_password         => $rabbit_password,
@@ -328,6 +339,13 @@ class openstack::controller (
     rabbit_node_ip_address  => $rabbit_node_ip_address,
     rabbit_port             => $rabbit_port,
     rabbit_ha_virtual_ip    => $rabbit_ha_virtual_ip,
+    # Qpid
+    qpid_password           => $qpid_password,
+    qpid_user               => $qpid_user,
+    qpid_cluster            => $qpid_cluster,
+    qpid_nodes              => $qpid_nodes,
+    qpid_port               => $qpid_port,
+    qpid_node_ip_address    => $qpid_node_ip_address,
     # Glance
     glance_api_servers      => $glance_api_servers,
     # General
@@ -346,23 +364,26 @@ class openstack::controller (
   if $cinder {
     if !defined(Class['openstack::cinder']) {
       class {'openstack::cinder':
-        sql_connection       => "mysql://${cinder_db_user}:${cinder_db_password}@${db_host}/${cinder_db_dbname}?charset=utf8",
-        rabbit_password      => $rabbit_password,
-        rabbit_host          => false,
-        rabbit_nodes         => $rabbit_nodes,
-        volume_group         => $cinder_volume_group,
-        physical_volume      => $nv_physical_volume,
-        manage_volumes       => $manage_volumes,
-        enabled              => true,
-        glance_api_servers   => "${service_endpoint}:9292",
-        auth_host            => $service_endpoint,
-        bind_host            => $api_bind_address,
-        iscsi_bind_host      => $cinder_iscsi_bind_addr,
-        cinder_user_password => $cinder_user_password,
-        use_syslog           => $use_syslog,
-        cinder_rate_limits   => $cinder_rate_limits,
-        rabbit_ha_virtual_ip => $rabbit_ha_virtual_ip,
-      }
+      sql_connection       => "mysql://${cinder_db_user}:${cinder_db_password}@${db_host}/${cinder_db_dbname}?charset=utf8",
+      queue_provider       => $queue_provider,
+      rabbit_password      => $rabbit_password,
+      rabbit_host          => false,
+      rabbit_nodes         => $rabbit_nodes,
+      qpid_password        => $qpid_password,
+      qpid_user            => $qpid_user,
+      qpid_nodes           => $qpid_nodes,
+      volume_group         => $cinder_volume_group,
+      physical_volume      => $nv_physical_volume,
+      manage_volumes       => $manage_volumes,
+      enabled              => true,
+      glance_api_servers   => "${service_endpoint}:9292",
+      auth_host            => $service_endpoint,
+      bind_host            => $api_bind_address,
+      iscsi_bind_host      => $cinder_iscsi_bind_addr,
+      cinder_user_password => $cinder_user_password,
+      use_syslog           => $use_syslog,
+      cinder_rate_limits   => $cinder_rate_limits,
+      rabbit_ha_virtual_ip => $rabbit_ha_virtual_ip,
     }
   } else { 
     if $manage_volumes {
@@ -376,12 +397,12 @@ class openstack::controller (
         physical_volume  => $nv_physical_volume,
       }
     }
-  } 
+  }
 
   if !defined(Class['memcached']){
     class { 'memcached':
       #listen_ip => $api_bind_address,
-    } 
+    }
   }
 
   ######## Horizon ########
